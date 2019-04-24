3766d192f38b7ba9386a85750635b153ca36bd11
Alexander Chan <alexander.chan088@gmail.com>
2019-04-24 16:38:37 -0500

    added menu, user commands, cart properties:

---
+++ cart_class.py
@@ -0,0 +1,33 @@
+
+
+
+class Cart(object):
+    def __init__(self):
+        self.cart = []
+        self.empty = True
+    
+    def is_empty(self, state):
+        self.empty = state
+    
+    def empty_cart(self):
+        self.cart = []
+
+    def add_to_cart(self, product):
+        self.cart.append(product)
+    
+    def view_cart(self):
+        if len(self.cart) != 0:
+            print("+   ", "", "", "", "", "+", sep="\t")
+            print("|", "CURRENT CART", "", "", "|", sep="\t")
+
+            for product in self.cart:
+                print("|", str(product.code), str(product.name),
+                        "$"+str(product.price), "|", sep="\t")
+
+            print("+   ", "---", "---", "---", "+", sep="\t")
+        
+            
+    
+    def checkout(self):
+        pass
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 #import dependencies
 from product_class import Product
+from cart_class import Cart
 
-#given a file with products (comma delimited)
-#ex - CH1,Chai,3.11
-#build the products and return them as a list of objects
+#INPUT: A file with products (comma delimited)
+    #ex - CH1,Chai,3.11
+#OUTPUT: Return list of product objects
 def build_products(in_file):
 
         #create a list to hold the created objects
@@ -15,20 +16,20 @@ def build_products(in_file):
             #read in all the products until we hit the end
             while product != "":
                 #split the string into a list of elements
-                # [prod_id, prod_name, prod_price]
+                # [prod_code, prod_name, prod_price]
                 product_details = product.split(",")
 
                 #only attempt to create an object if there are three elements in the list
                 #exception will be thrown if the 3rd object is not a number 
                 # ie - you can't have a string as a price
                 if len(product_details) == 3:
-                    prod_id = product_details[0]
+                    prod_code = product_details[0]
                     prod_name = product_details[1]
-                    prod_price = float(product_details[2])
-                    product_list.append(Product(prod_id, \
+                    prod_price = format(float(product_details[2]),".2f")
+                    product_list.append(Product(prod_code, \
                                                 prod_name, \
                                                 prod_price))
-                    #print("Item Created: " + str(prod_id))
+                    #print("Item Created: " + str(prod_code))
                 
                 #read in the next line from the file
                 product = in_file.readline()
@@ -40,24 +41,80 @@ def build_products(in_file):
         return product_list
 
             
-
+#INPUT: Product list
+#OUTPUT: Displays product menu
 def print_menu(product_list):
+    #print the product menu
     print("+   ", "", "", "", "+", sep="\t")
     print("|","TODAY'S PRODUCTS","|", sep ="\t")
     print("+   ","---","---","---","+", sep = "\t")
     print("|","Code","Name","Price","|", sep = "\t")
     print("+   ", "---", "---", "---", "+", sep="\t")
+    
+    #iterate through the list of product objects
     for product in product_list:
         print("|",str(product.code),str(product.name),"$"+str(product.price),"|", sep = "\t")
     print("+   ", "---", "---", "---", "+", sep="\t")
     print()
 
 
+#INPUT: NONE
+#OUTPUT: Display List of user commands
 def print_commands():
-    print("")
+    #print the choice menu
+    print("+   ", "", "", "", "", "+", sep="\t")
+    print("|", "USER COMMANDS", "","","|", sep="\t")
+    print("+   ", "---", "---", "---", "---", "+", sep="\t")
+    print("|", "Option 1: Add Item to Cart", "|", sep="\t")
+    print("|", "Option 2: View Cart", "", "|", sep="\t")
+    print("|", "Option 3: Checkout", "", "|", sep="\t")
+    print("+   ", "---", "---", "---", "---", "+", sep="\t")
 
-def take_command(command):
 
+#INPUT: NONE
+#OUTPUT: Return user command choice
+def take_command():
+    #call the print_commands functions to show the commands options
+    print_commands()
+    #ask user for choice
+    choice = input("\nEnter your choice (1, 2,or 3): ")
+    #validate that it is a correct choice
+    #if not, continue prompting until a correct choice is entered
+    while choice not in ["1", "2", "3"]:
+        choice = input("Invalid Choice. Enter your choice (1, 2,or 3): ")
+
+    #return the choice as an integer
+    return int(choice)
+
+
+#INPUT: Product list
+#OUTPUT: Calls a function to create the object given the product code, and returns the object
+def request_item_for_cart(product_list):
+    #collect a list of product codes
+    prod_code_list = [product.code for product in product_list]
+
+    #ask for user input
+    product_code = input("Enter your desired product code: ")
+    #convert input letters to uppercase
+    product_code = product_code.upper()
+    #validate that it is a valid product code
+    while product_code not in prod_code_list:
+        product_code = input("Invalid! Enter your desired product code: ")
+        #convert input letters to uppercase
+        product_code = product_code.upper()
+
+    return make_cart_item(product_code, product_list)
+
+
+#this next function is necessary because we don't want to overwrite the attributes of the original product
+#instead, we want to create a duplicate, so when we add discount features or make any adjustments, it doesn't affect the original object
+
+#INPUT: Product code, product list
+#OUTPUT: return an instantiation for Product to request_item_for_cart
+def make_cart_item(code, product_list):
+    for product in product_list:
+        if product.code == code:
+            return Product(code, product.name, product.price)
 
 
     
@@ -65,10 +122,46 @@ def take_command(command):
 
 
 def main():
+    #set a pointer to the file
     product_file = open('product_input.txt', 'r')
 
+    #build the list of available products
     products = build_products(product_file)
+
+    #print the Product Menu
     print_menu(products)
+
+    #declare an empty cart
+    cart = Cart()
+
+    #take in a user command
+    user_choice = take_command()
+
+    #flag that continues the program until the user requests to stop
+    interaction_flag = 99
+
+
+    while interaction_flag != -1:
+        #Add item to cart
+        if user_choice == 1:
+            cart.add_to_cart(request_item_for_cart(products))
+            print("Product Added!\n")
+            cart.is_empty(False)
+            #take another command
+            take_command()
+
+        #View Cart
+        elif user_choice == 2:
+            #view cart
+            pass
+            
+            #take another command
+            take_command()
+        #Checkout
+        elif user_choice == 3:
+            #checkout
+            pass
+
 
 
 
