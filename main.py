1533faba63d3a1c9c070dbe302ae17817a9bc4d7
Alexander Chan <alexander.chan088@gmail.com>
2019-04-24 13:27:10 -0500

    changes to main

---
+++ main.py
@@ -0,0 +1,74 @@
+#import dependencies
+from product_class import Product
+
+#given a file with products (comma delimited)
+#ex - CH1,Chai,3.11
+#build the products and return them as a list of objects
+def build_products(in_file):
+
+        #create a list to hold the created objects
+        product_list = []
+
+        #read in the first line
+        product = in_file.readline()
+        try:
+            #read in all the products until we hit the end
+            while product != "":
+                #split the string into a list of elements
+                # [prod_id, prod_name, prod_price]
+                product_details = product.split(",")
+
+                #only attempt to create an object if there are three elements in the list
+                #exception will be thrown if the 3rd object is not a number 
+                # ie - you can't have a string as a price
+                if len(product_details) == 3:
+                    prod_id = product_details[0]
+                    prod_name = product_details[1]
+                    prod_price = float(product_details[2])
+                    product_list.append(Product(prod_id, \
+                                                prod_name, \
+                                                prod_price))
+                    #print("Item Created: " + str(prod_id))
+                
+                #read in the next line from the file
+                product = in_file.readline()
+        #general exception handling
+        except Exception as err:
+            print("Error found!: ", err)
+            print()
+        #return the valid objects to main
+        return product_list
+
+            
+
+def print_menu(product_list):
+    print("+   ", "", "", "", "+", sep="\t")
+    print("|","TODAY'S PRODUCTS","|", sep ="\t")
+    print("+   ","---","---","---","+", sep = "\t")
+    print("|","Code","Name","Price","|", sep = "\t")
+    print("+   ", "---", "---", "---", "+", sep="\t")
+    for product in product_list:
+        print("|",str(product.code),str(product.name),"$"+str(product.price),"|", sep = "\t")
+    print("+   ", "---", "---", "---", "+", sep="\t")
+    print()
+
+
+    
+
+
+
+def main():
+    product_file = open('product_input.txt', 'r')
+
+    products = build_products(product_file)
+    print_menu(products)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  main()
---
+++ product_class.py
@@ -0,0 +1,21 @@
+
+
+#create a generic product class
+#based on specs, a product has 3 attributes
+#   1. Product Code
+#   2. Name
+#   3. Price
+class Product(object):
+    def __init__(self,code, name, price):
+        self.code = code
+        self.name = name
+        self.price = price
+        self.discount_price = 0
+
+    #return string method that shows the attributes for the object
+    def __str__(self):
+        # if self.discount_price == 0 and self.discount_name == "":
+        #     return str(self.product_code) + "\t\t\t\t\t" + str(self.discount) + str(self.price)
+        # if self.discount_price == 0:
+        #     return str(self.product_code) + "\t\t\t\t\t" + str(self.discount) + str(self.price)
+        return "Product code: " + str(self.product_code) + "\nName: " + str(self.name) + "\nPrice: " + str(self.price)
