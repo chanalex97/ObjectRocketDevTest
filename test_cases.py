66f89966208907879280b33ba6c7a2ae11f1eb35
Alexander Chan <alexander.chan088@gmail.com>
2019-04-28 11:55:19 -0500

    added starting materials for test cases

---
+++ test_cases.py
@@ -0,0 +1,9 @@
+from unittest import main, TestCase
+from product_class import Product
+from cart_class import Cart
+from promotions_class import Promotions
+
+
+class main_func(TestCase):
+    def test_1(self):
+        pass
