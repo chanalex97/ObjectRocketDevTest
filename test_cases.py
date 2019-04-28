f09f87f6c75568e7dd9f39ed5552e9cd40be134f
Alexander Chan <alexander.chan088@gmail.com>
2019-04-28 15:16:05 -0500

    finished unit tests

--- test_cases.py
+++ test_cases.py
@@ -66,42 +66,6 @@ class cart_class_tests(TestCase):
 # PROMOTION CLASS TESTS #
 #########################
 class promotion_class_tests(TestCase):
-    #instantiate objects that will be used for promotion test cases
-
-    #products
-
-    apple2 = Product("AP1", "Apples", 6.00)
-    apple3 = Product("AP1", "Apples", 6.00)
-
-
-    chai1 = Product("CH1", "Chai", 3.11)
-    chai2 = Product("CH1", "Chai", 3.11)
-
-    milk1 = Product("MK1", "Milk", 4.75)
-    milk2 = Product("MK1", "Milk", 4.75)
-
-    oatmeal1 = Product("OM1", "Oatmeal", 3.69)
-    oatmeal2 = Product("OM1", "Oatmeal", 3.69)
-
-    def test_product_types(self):
-        #make sure products are actually products
-
-        self.assertIsInstance(self.apple2, Product)
-        self.assertIsInstance(self.apple3, Product)
-
-
-
-        self.assertIsInstance(self.chai1, Product)
-        self.assertIsInstance(self.chai2, Product)
-
-        self.assertIsInstance(self.milk1, Product)
-        self.assertIsInstance(self.milk2, Product)
-
-        self.assertIsInstance(self.oatmeal1, Product)
-        self.assertIsInstance(self.oatmeal2, Product)
-
-    #promotions
-    promos = Promotions()
 
     #test is to make sure that given 4 coffees,
     #2 are discounted and 2 are not
@@ -138,8 +102,11 @@ class promotion_class_tests(TestCase):
         self.assertEqual(coffee3.discount.get("BOGO"), None)
         self.assertEqual(coffee4.discount.get("BOGO"), None)
         self.assertEqual(apple1.discount.get("BOGO"), None)
+
+        #promotions
+        promos = Promotions()
         #apply the promotion rules to the cart
-        self.promos.is_bogo(cart)
+        promos.is_bogo(cart)
         #verify that the correct products are adjusted
         self.assertEqual(coffee1.discount.get("BOGO"), None)
         self.assertEqual(list(coffee2.discount.keys())[0], "BOGO")
@@ -151,9 +118,120 @@ class promotion_class_tests(TestCase):
         self.assertEqual(coffee4.discount.get("BOGO"), coffee4.price * -1)
 
 
+    #test is to make sure that given 4 apples and 1 oatmeal (APPL and APOM discounts applied),
+    #3 of the apples will be $4.50 and the 4th will be $3.00, Oatmeal unchanged
+    def test_appl_apom_3discounted_1halfoff(self):
+        #instantiate products
+        apple1 = Product("AP1", "Apples", 6.00)
+        apple2 = Product("AP1", "Apples", 6.00)
+        apple3 = Product("AP1", "Apples", 6.00)
+        apple4 = Product("AP1", "Apples", 6.00)
+        oatmeal1 = Product("OM1", "Oatmeal", 3.69)
 
+        #make sure the products are actually Product objects
+        self.assertIsInstance(apple1, Product)
+        self.assertIsInstance(apple2, Product)
+        self.assertIsInstance(apple3, Product)
+        self.assertIsInstance(apple4, Product)
+        self.assertIsInstance(oatmeal1, Product)
 
+        #cart object instantiaion
+        cart = Cart()
+        self.assertIsInstance(cart, Cart)
 
+        #add items to the cart
+        cart.add_to_cart(apple1)
+        cart.add_to_cart(apple2)
+        cart.add_to_cart(apple3)
+        cart.add_to_cart(apple4)
+        cart.add_to_cart(oatmeal1)
+
+        #make sure no preexisting APPL or APOM deals exist in the products
+        self.assertEqual(apple1.discount.get("APPL"), None)
+        self.assertEqual(apple1.discount.get("APOM"), None)
+        self.assertEqual(apple2.discount.get("APPL"), None)
+        self.assertEqual(apple1.discount.get("APOM"), None)
+        self.assertEqual(apple3.discount.get("APPL"), None)
+        self.assertEqual(apple1.discount.get("APOM"), None)
+        self.assertEqual(apple4.discount.get("APPL"), None)
+        self.assertEqual(oatmeal1.discount.get("APPL"), None)
+        self.assertEqual(oatmeal1.discount.get("APOM"), None)
+
+        #promotions
+        promos = Promotions()
+        #apply the promotion rules to the cart
+        promos.is_appl(cart)
+        promos.is_apom(cart)
+
+        #verify that the correct products are adjusted
+        #only the first apple should have both discounts, the other 3 shouldn't
+        self.assertDictContainsSubset(apple1.discount, {'APPL':-1.50,'APOM':apple1.price * -0.50})
+
+        self.assertDictContainsSubset(apple2.discount, {'APPL': -1.50})
+        self.assertEqual(apple2.discount.get('APOM'), None)
+
+        self.assertDictContainsSubset(apple3.discount, {'APPL': -1.50})
+        self.assertEqual(apple3.discount.get('APOM'), None)
+
+        self.assertDictContainsSubset(apple4.discount, {'APPL': -1.50})
+        self.assertEqual(apple4.discount.get('APOM'), None)
+
+        #oatmeal remains unchanged
+        self.assertEqual(oatmeal1.discount.get('APPL'), None)
+        self.assertEqual(oatmeal1.discount.get('APOM'), None)
+
+        #set the best discounts for each of the products
+        for product in cart.cart:
+            product.set_greatest_discount()
+        #check that the best discounts are correct
+        self.assertDictContainsSubset(apple1.best_discount, {'APOM': apple1.price * -0.50})
+        self.assertDictContainsSubset(apple2.best_discount, {'APPL': -1.50})
+        self.assertDictContainsSubset(apple3.best_discount, {'APPL': -1.50})
+        self.assertDictContainsSubset(apple4.best_discount, {'APPL': -1.50})
+        #oatmeal has no discounts
+        self.assertEqual(len(oatmeal1.discount.keys()), 0)
+
+    #test is to make sure that given 2 oatmeals and 2 chais (CHMK discount),
+    #the CHMK discount will only be used once (applied on 1 milk, not both)
+    def test_chmk(self):
+        #instantiate the products
+        chai1 = Product("CH1", "Chai", 3.11)
+        chai2 = Product("CH1", "Chai", 3.11)
+        milk1 = Product("MK1", "Milk", 4.75)
+        milk2 = Product("MK1", "Milk", 4.75)
+
+        #make sure they are type Product
+        self.assertIsInstance(chai1, Product)
+        self.assertIsInstance(chai2, Product)
+        self.assertIsInstance(milk1, Product)
+        self.assertIsInstance(milk2, Product)
+
+        #cart object instantiaion
+        cart = Cart()
+        self.assertIsInstance(cart, Cart)
+
+        #add items to the cart
+        cart.add_to_cart(chai1)
+        cart.add_to_cart(chai2)
+        cart.add_to_cart(milk1)
+        cart.add_to_cart(milk2)
+
+        #make sure no preexisting CHMK deal exists in the products
+        self.assertEqual(chai1.discount.get("CHMK"), None)
+        self.assertEqual(chai2.discount.get("CHMK"), None)
+        self.assertEqual(milk1.discount.get("CHMK"), None)
+        self.assertEqual(milk2.discount.get("CHMK"), None)
+
+        #promotions
+        promos = Promotions()
+        #apply the promotion rules to the cart
+        promos.is_chmk(cart)
+
+        #verify that the correct products are adjusted
+        self.assertDictContainsSubset(milk1.discount, {'CHMK': milk1.price * -1})
+        self.assertEqual(milk2.discount.get('CHMK'), None)
+        self.assertEqual(chai1.discount.get('CHMK'), None)
+        self.assertEqual(chai2.discount.get('CHMK'), None)
 
 if __name__ == '__main__':
     main()
